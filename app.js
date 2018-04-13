0b5bbe4ac718e511659f991e6cb4e1668f7cfad0
Kevin Alwell <alwellkevin44@gmail.com>
2018-04-13 11:39:35 -0400

    start server

---
+++ app.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 3000;
+const app = express();
+
+const nexmo = require('./nexmo');
+
+app.use(bodyParser.json({
+    type: 'application/json'
+}));
+
+app.all('/nexmo', (req, res) => {
+    console.log(req);
+    res.sendStatus(200);
+});
+
+// Start server
+app.listen(port, () => {
+    console.log('Express server started on port ' + port);
+})
